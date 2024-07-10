da28729af59a1c0c12b8128a538adb9ee8c11c12
Umer Farooq <ufarooq211339@gmail.com>
2024-07-11 02:08:19 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  7 01:36:01 2024
+
+@author: PMYLS
+"""
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import pickle
+import json
+
+app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class model_input(BaseModel):
+    Pregnancies: int
+    Glucose: int
+    BloodPressure: int
+    SkinThickness: int
+    Insulin: int
+    BMI: float
+    DiabetesPedigreeFunction: float
+    Age: int
+    
+#loading saved model
+diabetes_model = pickle.load(open('D:/Software Engineering/ML/ML Model as api/Python code/diabetes_model.sav', 'rb'))
+
+@app.post('/diabetes_prediction')
+def diabetes_pred(input_parameters: model_input):
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
+    
+    preg = input_dictionary['Pregnancies']
+    glu = input_dictionary['Glucose']
+    bp = input_dictionary['BloodPressure']
+    skin = input_dictionary['SkinThickness']
+    insulin = input_dictionary['Insulin']
+    bmi = input_dictionary['BMI']
+    dpf = input_dictionary['DiabetesPedigreeFunction']
+    age = input_dictionary['Age']
+    
+    input_list = [preg, glu, bp, skin, insulin, bmi, dpf, age]
+    
+    prediction = diabetes_model.predict([input_list])
+    
+    if prediction[0] == 0:
+        return 'The person is not diabetic'
+    
+    else:
+        return 'The person is Diabetic'
+    
